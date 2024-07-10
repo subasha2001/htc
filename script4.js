39d4b2066157206b19ec84c9d30360e6274e7522
Subash A <168972847+subasha2001@users.noreply.github.com>
2024-07-10 11:16:17 +0530

    Add files via upload

---
+++ script4.js
@@ -0,0 +1,7 @@
+function openNavBar(){
+    if(document.getElementById("nav-bar").classList.contains('alive-nav')){
+      document.getElementById("nav-bar").classList.remove('alive-nav');
+    }else{
+      document.getElementById("nav-bar").classList.add('alive-nav');
+    }
+  }
